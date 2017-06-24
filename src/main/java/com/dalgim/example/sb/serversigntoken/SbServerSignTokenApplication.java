92126881b1fb5075247919f09104269d445b48e1
dalgim <dalgiewiczmateusz@gmail.com>
2017-06-25 00:05:11 +0200

    Initial commit

---
+++ src/main/java/com/dalgim/example/sb/serversigntoken/SbServerSignTokenApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.serversigntoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbServerSignTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbServerSignTokenApplication.class, args);
+	}
+}
